40d0bf91d45cdf67372f1f1cee7cde68d62e28b4
Ethun Tcg <125822203+tcgesan@users.noreply.github.com>
2024-10-15 10:28:41 +0600

    test

--- script.js
+++ script.js
@@ -444,3 +444,17 @@ function next() {
         window.location.href = 'index.html';
     }, 150); // Adjust delay as needed
 }
+
+
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('service-worker.js')
+            .then((registration) => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            })
+            .catch((error) => {
+                console.log('ServiceWorker registration failed: ', error);
+            });
+    });
+}
