97a99d5fc7e1b5c194a8c73cb1738a1d058adc95
Ethun Tcg <125822203+tcgesan@users.noreply.github.com>
2024-10-17 19:14:43 +0600

    Add files via upload

--- tense.js
+++ tense.js
@@ -4,12 +4,12 @@ let timeLeft = 20;
 
 let questions = [
     { 
-        question: " ✨ . One of the professor's greatest attributes is -------- ?", 
+        question: " ✨ . One of the professor's greatest attributes is -------- .", 
         options: ["When he gives lectures", "how in the manner that he lectures", "the way to give lectures", "his ability to lecture"], 
         answer: 3// Index of the correct answer
     },
     { 
-        question: " ✨ . I ___ to a foreign country ?", 
+        question: " ✨ . I ___ to a foreign country.", 
         options: ["was nerver been", "am never gone", "have never been", "am never been"], 
         answer: 2
     },
@@ -25,8 +25,9 @@ let questions = [
         options: ["I know him for two years", "I have been knowing him for two years", "I have know him for two years", "I have known him since years"], 
         answer: 2
     },
+    
     { 
-        question: " ✨ . ___ school very young, he is very successful ?", 
+        question: " ✨ . ___ school very young, he is very successful.", 
         options: ["Although leaving", "In spite of he left", "Because of", "In spite of leaving"], 
         answer: 3 
     },
