27b985e8639dca8a184ff5395c946db0b1295bcc
ethun <ethuntcg@gmail.com>
2024-10-24 08:24:37 +0600

    update

--- script.js
+++ script.js
@@ -1255,6 +1255,21 @@ function voca() {
     }, 150); // Adjust delay as needed
 }
 
+
+
+function audioBook() {
+    var sound = document.getElementById("clickSound");
+    sound.play();
+
+    // Wait for the sound to play for at least 500ms before redirecting
+    setTimeout(function() {
+        window.location.href = '../HTMLS/updating.html';
+    }, 150); // Adjust delay as needed
+}
+
+
+
+
 function gk() {
     var sound = document.getElementById("clickSound");
     sound.play();
